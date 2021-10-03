2f92da0e6749eba587b1dba4503579078f6f2188
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-10-04 01:03:23 +0300

    Add antd css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.scss';
 import { Provider } from 'react-redux';
 import { appWithTranslation } from 'next-i18next';
 import store from '../store/store';
+import 'antd/dist/antd.css';
 
 function MyApp({ Component, pageProps }) {
   return (
