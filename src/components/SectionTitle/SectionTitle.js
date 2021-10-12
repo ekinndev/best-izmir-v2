ae560abc2a2d1e019292647266ed9bb7d7a700ed
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-10-12 21:03:46 +0300

    Add section Title

---
+++ src/components/SectionTitle/SectionTitle.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useTranslation } from 'next-i18next';
+import styles from './SectionTitle.module.scss';
+
+const SectionTitle = ({ titleId, type }) => {
+  const { t } = useTranslation('common');
+
+  return React.createElement(type, { className: styles.sectionTitle }, t(titleId));
+};
+
+SectionTitle.propTypes = {};
+
+export default SectionTitle;
