282ada560842e4182bad2b798b130f58913f864d
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-10-12 21:04:27 +0300

    Add constants for homepage

---
+++ src/constants/Home.js
@@ -0,0 +1,45 @@
+/* eslint-disable import/prefer-default-export */
+export const homePageEvents = [
+  {
+    titleKey: 'events.ebecTitle',
+    contentKey: 'events.ebecContent',
+    link: '/events/ebec',
+    imageLink: 'https://via.placeholder.com/464x150',
+  },
+  {
+    titleKey: 'events.mwIzmirTitle',
+    contentKey: 'events.mwIzmirContent',
+    link: '/events/ebec',
+    imageLink: 'https://via.placeholder.com/464x150',
+  },
+  {
+    titleKey: 'events.yfsTitle',
+    contentKey: 'events.yfsContent',
+    link: '/events/ebec',
+    imageLink: 'https://via.placeholder.com/464x150',
+  },
+  {
+    titleKey: 'events.beinciTitle',
+    contentKey: 'events.beinciContent',
+    link: '/events/ebec',
+    imageLink: 'https://via.placeholder.com/464x150',
+  },
+  {
+    titleKey: 'events.courseTitle',
+    contentKey: 'events.courseContent',
+    link: '/events/ebec',
+    imageLink: 'https://via.placeholder.com/464x150',
+  },
+  {
+    titleKey: 'events.otherTitle',
+    contentKey: 'events.otherContent',
+    link: '/events/ebec',
+    imageLink: 'https://via.placeholder.com/464x150',
+  },
+];
+
+export const homePagePartners = [
+  { id: '1', imageLink: 'https://via.placeholder.com/464x150' },
+  { id: '2', imageLink: 'https://via.placeholder.com/464x150' },
+  { id: '3', imageLink: 'https://via.placeholder.com/464x150' },
+];
