aac1ad865a8570d59d126500ae33b410b3e3e7c1
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-10-13 16:50:29 +0300

    Use only one instance of firestore db

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,23 +2,8 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import { FirebaseAdapter } from '@next-auth/firebase-adapter';
+import firestore from '../../../utils/db';
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyDc9mBzcwAdsRyKjYY8i1Akh1zxSAWXDjk',
-  authDomain: 'best-izmir-v2.firebaseapp.com',
-  projectId: 'best-izmir-v2',
-  storageBucket: 'best-izmir-v2.appspot.com',
-  messagingSenderId: '987079972464',
-  appId: '1:987079972464:web:a93a7b14d251f990e7c6b5',
-};
-
-const firestore = (firebase.apps[0] ?? firebase.initializeApp(firebaseConfig)).firestore();
-
-// For more information on each option (and a full list of options) go to
-// https://next-auth.js.org/configuration/options
 export default NextAuth({
   // https://next-auth.js.org/configuration/providers
   providers: [
@@ -34,6 +19,21 @@ export default NextAuth({
         return true;
       }
       return false;
+    },
+    async session(props) {
+      const userEmail = props.user.email;
+      const querySnapshot = await firestore.collection('users').where('isAdmin', '==', true).get();
+      let firebaseUser = {};
+
+      querySnapshot.forEach(doc => {
+        const data = doc.data();
+
+        if (data.email === userEmail) {
+          firebaseUser = { ...data, id: doc.id };
+        }
+      });
+
+      return { ...props, user: { ...props.user, userId: firebaseUser.id, isAdmin: firebaseUser.isAdmin } };
     },
   },
 });
