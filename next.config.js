8c5b8e01d7b6bbd0468dc930bec57cd50b113ddd
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-10-13 13:51:31 +0300

    Add Instagram section

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const { i18n } = require('./next-i18next.config');
 module.exports = {
   i18n,
   images: {
-    domains: ['via.placeholder.com'],
+    domains: ['via.placeholder.com', 'scontent.cdninstagram.com'],
   },
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,18 @@
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useRouter } from 'next/router';
-import { Card, Col, Layout as ALayout, Row } from 'antd';
+import { Avatar, Card, Col, Layout as ALayout, Row } from 'antd';
 import Image from 'next/image';
+import axios from 'axios';
 import Hero from '../components/Hero/Hero';
 import SectionTitle from '../components/SectionTitle/SectionTitle';
 import NextLink from '../components/NextLink/NextLink';
 import { homePageEvents, homePagePartners } from '../constants/Home';
 
-export default function Home() {
+export default function Home({ instagramFeedData }) {
   const router = useRouter();
   const { t } = useTranslation('common');
+  const { Meta } = Card;
 
   return (
     <ALayout>
@@ -37,16 +39,53 @@ export default function Home() {
         <SectionTitle titleId="partnersSectionTitleHome" type="h2" />
         <Row gutter={[16, 24]}>
           {homePagePartners.map(partner => (
-            <Col sm={{ span: 24 }} md={{ span: 12 }} lg={{ span: 8 }}>
+            <Col sm={{ span: 24 }} md={{ span: 12 }} lg={{ span: 8 }} key={partner.id}>
               <Image src={partner.imageLink} width={464} height={150} />
             </Col>
           ))}
         </Row>
       </section>
+      {instagramFeedData?.data?.length > 0 && (
+        <section className="instagram">
+          <SectionTitle titleId="instagramSectionTitleHome" type="h2" />
+          <Row gutter={[16, 24]}>
+            {instagramFeedData.data.slice(0, 6).map(post => (
+              <Col sm={{ span: 24 }} md={{ span: 12 }} lg={{ span: 8 }} key={post.id}>
+                <NextLink href={post.permalink}>
+                  <Card
+                    bordered
+                    style={{ minHeight: '525px' }}
+                    cover={<Image src={post.media_url} width={1080} height={608} />}
+                  >
+                    <Meta
+                      avatar={
+                        // eslint-disable-next-line max-len
+                        <Avatar src="/images/logo.jpg" />
+                      }
+                      title={`@${post.username}`}
+                    />
+                    {`${post.caption.slice(0, 400)}...`}
+                  </Card>
+                </NextLink>
+              </Col>
+            ))}
+          </Row>
+        </section>
+      )}
     </ALayout>
   );
 }
 
-export const getServerSideProps = async ({ locale }) => ({
-  props: { ...(await serverSideTranslations(locale, ['common', 'menu', 'hero'])) },
-});
+export const getServerSideProps = async ({ locale }) => {
+  const instagramFeed = await axios.get(
+    // eslint-disable-next-line max-len
+    `https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type,permalink,thumbnail_url,timestamp,username&access_token=${process.env.INSTAGRAM_TOKEN}`,
+  );
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common', 'menu', 'hero'])),
+      instagramFeedData: instagramFeed.data || null,
+    },
+  };
+};
