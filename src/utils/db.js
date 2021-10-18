37e268c3fd6bebcc5942d48c4fd2475e5bf6ec3f
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-10-19 00:30:11 +0300

    Add google analytics

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/analytics';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDc9mBzcwAdsRyKjYY8i1Akh1zxSAWXDjk',
@@ -8,8 +9,14 @@ const firebaseConfig = {
   storageBucket: 'best-izmir-v2.appspot.com',
   messagingSenderId: '987079972464',
   appId: '1:987079972464:web:a93a7b14d251f990e7c6b5',
+  measurementId: 'G-3P11PQZKQ3',
 };
+const app = firebase.apps[0] ?? firebase.initializeApp(firebaseConfig);
 
-const firestore = (firebase.apps[0] ?? firebase.initializeApp(firebaseConfig)).firestore();
+const firestore = app.firestore();
+
+if (typeof window !== 'undefined') {
+  firebase.analytics(app);
+}
 
 export default firestore;
