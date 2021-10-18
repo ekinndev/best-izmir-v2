2948f4239d5830f28ff6946939edc77192b37ad8
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-10-18 15:46:35 +0300

    Faq page

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { Layout, Collapse } from 'antd';
+import { useTranslation } from 'next-i18next';
+import Hero from '../components/Hero/Hero';
+import SectionTitle from '../components/SectionTitle/SectionTitle';
+import FAQ_QUESTIONS from '../constants/Faq';
 
-const Faq = props => <div>Faq</div>;
+const Faq = props => {
+  const { Panel } = Collapse;
+
+  const { t } = useTranslation('faq');
+
+  return (
+    <Layout>
+      <Hero titleId="heroTitleFAQ" contentId="heroContentFaq" image="https://via.placeholder.com/1430x500" />
+      <SectionTitle type="h2" titleId="sectionTitleFAQ" />
+      <Collapse>
+        {FAQ_QUESTIONS.map(question => (
+          <Panel header={t(question.questionKey)} key={question.questionKey}>
+            <p>{t(question.answerKey)}</p>
+          </Panel>
+        ))}
+      </Collapse>
+      ,
+    </Layout>
+  );
+};
 
 Faq.propTypes = {};
 
 export const getServerSideProps = async ({ locale }) => ({
-  props: { ...(await serverSideTranslations(locale, ['common', 'menu', 'pages'])) },
+  props: { ...(await serverSideTranslations(locale, ['common', 'menu', 'pages', 'faq', 'hero'])) },
 });
 
 export default Faq;
