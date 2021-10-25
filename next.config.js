dde6c80143783f52901af6c33fde57fca0ba3f23
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-10-25 17:08:49 +0300

    Add security headers

--- next.config.js
+++ next.config.js
@@ -1,8 +1,47 @@
 const { i18n } = require('./next-i18next.config');
 
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+  },
+];
+
 module.exports = {
   i18n,
   images: {
     domains: ['via.placeholder.com', 'scontent.cdninstagram.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
   },
 };
