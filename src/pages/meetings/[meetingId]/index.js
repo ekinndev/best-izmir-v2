d660d78e55d126da26d6c81ea73dc158a94f0809
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2022-01-07 00:16:25 +0300

    Feature| add manuel participant to the meeting
    If there is a problem with participant's webcam admin may add participant as manuel

--- src/pages/meetings/[meetingId]/index.js
+++ src/pages/meetings/[meetingId]/index.js
@@ -4,10 +4,12 @@ import { getSession } from 'next-auth/client';
 import Head from 'next/head';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { Table, Input, Button, Space } from 'antd';
+import { Table, Input, Button, Space, Modal, Form, message } from 'antd';
 import { CSVLink } from 'react-csv';
 import { useTranslation } from 'next-i18next';
 import { useSelector } from 'react-redux';
+import firebase from 'firebase/app';
+import { v4 as uuidv4 } from 'uuid';
 import firestore from '../../../utils/db';
 import styles from './styles.module.scss';
 
@@ -15,8 +17,11 @@ const Meetings = () => {
   const router = useRouter();
   const { meetingId } = router.query;
   const [data, setData] = useState(null);
+  const [showModal, setShowModal] = useState(false);
   const [csvData, setCsvData] = useState(null);
+  const [loading, setLoading] = useState(false);
   const { t } = useTranslation('pages');
+  const { t: profileT } = useTranslation('profile');
   const loggedInUser = useSelector(state => state.common.userInfo);
 
   const handleSearch = (selectedKeys, confirm) => {
@@ -25,6 +30,41 @@ const Meetings = () => {
 
   const handleReset = clearFilters => {
     clearFilters();
+  };
+  const [manuelForm] = Form.useForm();
+
+  const addManuelUser = async values => {
+    const { participantName, participantEmail } = values;
+
+    const payload = {
+      email: participantEmail || 'Manuel',
+      name: participantName,
+      created: firebase.firestore.Timestamp.now().toDate().toLocaleString(),
+    };
+
+    if (!participantName) return false;
+
+    setLoading(true);
+
+    try {
+      const meeting = await firestore.collection('meetings').doc(meetingId);
+
+      const userMeeting = meeting.collection('attendees').doc(uuidv4());
+
+      await userMeeting.set(payload);
+
+      message.success(profileT('successAttentMeeting'));
+
+      return true;
+    } catch (e) {
+      message.error(profileT('unexpectedError'));
+
+      return false;
+    } finally {
+      setLoading(false);
+      manuelForm.resetFields();
+      setShowModal(false);
+    }
   };
 
   const getColumnSearchProps = dataIndex => ({
@@ -115,23 +155,48 @@ const Meetings = () => {
         JoinTime: userData.created,
       }));
 
-      setCsvData(tableCsvData);
+      if (!showModal) setCsvData(tableCsvData);
     };
 
     initFunc();
-  }, [meetingId]);
+  }, [meetingId, showModal]);
 
   return !loggedInUser?.isAdmin ? null : (
     <div className={styles.meetings}>
       <Head>
         <title>{`${t('meetingIdPageTitle')} ${meetingId} | ${t('bestIzmir')}`}</title>
       </Head>
+      <Modal
+        title="Manuel"
+        visible={showModal}
+        confirmLoading={loading}
+        onOk={() => manuelForm.submit()}
+        onCancel={() => setShowModal(false)}
+      >
+        <Form form={manuelForm} name="participant" onFinish={addManuelUser}>
+          <Form.Item label="Email" name="participantEmail">
+            <Input type="email" />
+          </Form.Item>
+          <Form.Item
+            label={profileT('name')}
+            name="participantName"
+            rules={[{ required: true, message: t('participantNameErrorMessage') }]}
+          >
+            <Input />
+          </Form.Item>
+        </Form>
+      </Modal>
       <div style={{ overflowX: 'auto', width: '100%' }}>
-        {csvData && (
-          <CSVLink filename={`${meetingId}.csv`} data={csvData}>
-            EXCEL
-          </CSVLink>
-        )}
+        <div className={styles.table_header}>
+          {csvData && (
+            <CSVLink filename={`${meetingId}.csv`} data={csvData}>
+              EXCEL
+            </CSVLink>
+          )}
+          <Button type="primary" onClick={() => setShowModal(true)}>
+            {profileT('addManuelButtonText')}
+          </Button>
+        </div>
         <Table columns={columns} dataSource={data} />
       </div>
     </div>
@@ -151,7 +216,7 @@ export const getServerSideProps = async ({ locale, ...ctx }) => {
     };
   }
   return {
-    props: { ...(await serverSideTranslations(locale, ['common', 'menu', 'pages'])) },
+    props: { ...(await serverSideTranslations(locale, ['common', 'menu', 'pages', 'profile'])) },
   };
 };
 export default Meetings;
