6c3abf75cbd9a7983a7b5c44a727baabcf57725b
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2022-01-16 14:25:46 +0300

    Add Dark theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,22 @@ import { Provider } from 'react-redux';
 import { appWithTranslation } from 'next-i18next';
 import { Provider as NProvider } from 'next-auth/client';
 import store from '../store/store';
-import 'antd/dist/antd.css';
 import Layout from '../components/Layout/Layout';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  useEffect(() => {
+    const handleTheme = (event: MediaQueryListEvent) => {
+      console.log(event.matches);
+    };
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', handleTheme);
+
+    return () => {
+      window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', handleTheme);
+    };
+  }, []);
   return (
     <NProvider session={session}>
       <Provider store={store}>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,16 @@ class MyDocument extends Document {
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
           <meta name="language" content="English" />
           <meta name="author" content="Best Izmir" />
-
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.18.3/antd.dark.min.css"
+            media="screen and (prefers-color-scheme: dark)"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.18.3/antd.min.css"
+            media="screen and (prefers-color-scheme: light)"
+          />
           <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
           <link rel="icon" type="image/png" href="/favicon/favicon.png" />
         </Head>
