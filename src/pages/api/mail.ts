c14d71ad0161fd6ce1e29d18c9f72aad8b4939a6
yektayuksel <yekta.yuksel@gmail.com>
2022-02-27 19:04:53 +0300

    fix sendgrid api

--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -2,12 +2,8 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const sgMail = require('@sendgrid/mail');
 
-interface NextApiRequestAndSendGrid extends NextApiRequest {
-  sgMail: any;
-}
-
-export default async (req: NextApiRequestAndSendGrid, res: NextApiResponse) => {
-  req.sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const payload = `Site üzerinden ${new Date().toDateString()} Tarihinde Bir Form Dolduruldu.<br/>
   Isim:${req.body?.name}<br/>
   Email:${req.body?.email}<br/>
