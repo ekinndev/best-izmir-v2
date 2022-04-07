c6c1d3873db05b0745ff11d30169c67d6da8c247
yektayuksel <yekta.yuksel@gmail.com>
2022-04-07 13:15:46 +0300

    add course description

---
+++ src/pages/events/best-courses-in-autumn.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { GetServerSideProps } from 'next';
+import { useTranslation } from 'next-i18next';
+import styles from '../events/best-courses-in-autumn.module.scss';
+import Image from 'next/image';
+import courseLogo from '../../assets/best-courses-in-autumn/BESTCoursesLogo.png';
+import { Row, Col, Tabs } from 'antd';
+const BestCourse = () => {
+  const { t } = useTranslation('ac');
+  const { TabPane } = Tabs;
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.imageContainer}>
+        <Image src={courseLogo} placeholder="blur" alt="course-logo" />
+      </div>
+      <Row justify="center">
+        <Col xs={24} md={24}>
+          <Tabs className={styles.tabStyle} defaultActiveKey="1" centered>
+            <TabPane className={styles.tabContent} tab="About Course" key="1">
+              <div>
+                <div className={styles.courseHeader}>
+                  <h1>{t('courseTitle')}</h1>
+                </div>
+                <p>{t('courseDescription1')}</p>
+                <p>{t('courseDescription2')}</p>
+                <p>{t('courseDescription3')}</p>
+                <p>{t('courseDescription4')}</p>
+                <p>{t('courseDescription5')}</p>
+                <p>{t('courseDescription6')}</p>
+                <p>{t('courseDescription7')}</p>
+                <p>{t('courseDescription8')}</p>
+                <p>{t('courseDescription9')}</p>
+              </div>
+            </TabPane>
+            <TabPane className={styles.tabContent} tab="Schedule" key="2"></TabPane>
+            <TabPane className={styles.tabContent} tab="Survival Guide" key="3"></TabPane>
+            <TabPane className={styles.tabContent} tab="F.A.Q" key="4"></TabPane>
+          </Tabs>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ locale = 'en' }) => ({
+  props: { ...(await serverSideTranslations(locale, ['common', 'menu', 'pages', 'ac'])) },
+});
+
+export default BestCourse;
